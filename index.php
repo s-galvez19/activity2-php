76231f0058d1ccfa243a9dbbf5904ae7636b3c19
unknown <sgalvez@techlaunch.io>
2019-04-29 20:49:31 -0400

    first commit

--- index.php
+++ index.php
@@ -4,24 +4,32 @@
 $string = "the,cat,jumped,over,the,fence,at,a,speed,never,imagined";  
 $str_arr = preg_split ("/\,/", $string);  
 print_r($str_arr); 
+echo  "<br>";
 
 // 2.Convert each word with three letters of less into uppercase.
 
 $str = "the cat jumped over the fence at a speed never imagined";
 $str = strtoupper($str);
 echo $str; 
+echo "<br>";
 
-$array = [...];
-shuffle($array);
 
-// 3.Get two random words and delete them from the array.
+// // 3.Get two random words and delete them from the array.
 
-$input = array("The", "cat", "jumped", "over", "the", "fence", "at", "speed", "never", "imaged");
+ $input = array("The", "cat", "jumped", "over", "the", "fence", "at", "speed", "never", "imaged");
 $rand_keys = array_rand($input, 2);
-echo $input[$rand_keys[0]] . "\n";
-echo $input[$rand_keys[1]] . "\n";
+ echo $input[$rand_keys[0]] . "\n";
+ echo $input[$rand_keys[1]] . "\n";
+ echo "<br>";
 
-// 5.Echo the following text: “The text is ‘$finalText’ and it has X words”.
 
-echo "The text is ‘$finalText’ and it has X words";
+// // 5.Echo the following text: “The text is ‘$finalText’ and it has X words”.
+$finalText = "Steven Galvez";
+$x = str_word_count($finalText);
+echo "The text is $finalText and it has $x words";
+echo "<br>";
+
 ?>
+
+
+
